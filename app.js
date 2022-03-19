86918edfca06f6a31f0da70fbf9ab23739cd2ec0
DevRamiD <ka_boukef@esi.dz>
2022-03-19 21:08:55 +0100

    make it better

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.set('view engine','ejs');
 app.use(express.static('public'));
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 3000)
 
 app.get("/", (req ,res) => {
     let style = "color:green"
