e3842d49409785b874d45e7611d51db2c15aa85e
DevRamiD <ka_boukef@esi.dz>
2022-03-19 21:05:25 +0100

    make it better

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.listen(3000)
+
+app.get("/", (req ,res) => {
+    let style = "color:green"
+    let monday = ""
+    if (new Date().getDay()== 1) {
+        monday = "YES"
+    } else {
+        monday = "NO"
+        style = "color:red"
+    }
+    res.render('index',{monday,style});
+
+})
